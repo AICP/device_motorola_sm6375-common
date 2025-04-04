ee762c3335a98d2612b01812296978e748e4ae19
Julian Veit <Claymore1298@gmail.com>
2025-04-04 16:08:34 +0200

    sm6375-common: switch to new kernel
    Change-Id: I7d16252877d17716b73656c9a1089c46108f0a49

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,8 +44,9 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
-TARGET_KERNEL_SOURCE := kernel/motorola/msm-5.4
+TARGET_KERNEL_NO_GCC := true
+TARGET_KERNEL_SOURCE := kernel/motorola/sm6375
+TARGET_KERNEL_CONFIG := vendor/holi-qgki_defconfig vendor/ext_config/moto-holi.config
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
