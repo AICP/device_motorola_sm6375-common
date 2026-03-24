10fe8615ffab6297eec5291ac2d59144e8397504
Julian Veit <claymore1298@gmail.com>
2026-03-24 16:04:44 +0000

    Revert "sm6375-common: enable Smart Pixels overlay"
    This reverts commit a2c4a558fcea4f81bb765a6806a3b851cd0c150c.
    
    Change-Id: I6932929992d417598bbc02157d0ed5453ece97b7

--- common.mk
+++ common.mk
@@ -23,8 +23,7 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage \
-    $(LOCAL_PATH)/overlay-aicp
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
